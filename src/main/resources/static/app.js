11393ef851e665352cc22241ca3326c7e5cb1eab
Juan Carlos Alayon Molina <jkam86425@gmail.com>
2022-02-23 17:14:48 -0500

    Update app.js

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -1,10 +1,11 @@
 var weatherButton = document.getElementById("weatherButton");
 var respuesta = document.getElementById("temperature");
+var url = /consulta?lugar=" ;
 
 weatherButton.addEventListener('click', function (){
     respuesta.append('');
     var city = document.getElementById("name").value;
-    axios.get(city).then(res => {
+    axios.get(url + city).then(res => {
         console.log(res.data);
         respuesta.append(res.data);
     }).catch(ex => {
